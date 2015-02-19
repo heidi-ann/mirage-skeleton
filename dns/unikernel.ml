36a91eb4d66d5068a4b28257eb4d5908509305d8
Heidi-ann <hh360@cam.ac.uk>
2015-02-19 11:33:23 +0000

    refactoing and simplying

--- dns/unikernel.ml
+++ dns/unikernel.ml
@@ -1,20 +1,31 @@
 open Lwt
 open V1_LWT
 open Printf
+open Dns_server
 
 let listening_port = 5354
 
-let red fmt    = sprintf ("\027[31m"^^fmt^^"\027[m")
-let green fmt  = sprintf ("\027[32m"^^fmt^^"\027[m")
-let yellow fmt = sprintf ("\027[33m"^^fmt^^"\027[m")
-let blue fmt   = sprintf ("\027[36m"^^fmt^^"\027[m")
 
 module Main (C:CONSOLE) (K:KV_RO) (S:STACKV4) = struct
 
-  module U = S.UDPV4
+ module U = S.UDPV4
 
-  (** Note that a lot of this logic will eventually move into
-      a mirage-dns library, and is just here temporarily *)
+  let listener ~processor = 
+    fun ~src ~dst ~src_port buf ->
+          C.log_s c "got udp"
+          >>= fun () ->
+          let ba = Cstruct.to_bigarray buf in
+          let src' = (Ipaddr.V4 dst), listening_port in
+          let dst' = (Ipaddr.V4 src), src_port in
+          let obuf = (Io_page.get 1 :> Dns.Buf.t) in
+          process_query ba (Dns.Buf.length ba) obuf src' dst' processor
+          >>= function
+          | None ->
+            C.log_s c "No response"
+          | Some rba ->
+            let rbuf = Cstruct.of_bigarray rba in
+            U.write ~source_port:listening_port ~dest_ip:src ~dest_port:src_port udp rbuf
+
   let start c k s =
     lwt zonebuf = 
       K.size k "test.zone"
@@ -26,24 +37,8 @@ module Main (C:CONSOLE) (K:KV_RO) (S:STACKV4) = struct
         | `Error _ -> fail (Failure "test.zone error reading")
         | `Ok pages -> return (String.concat "" (List.map Cstruct.to_string pages))
     in
-    let open Dns_server in
     let process = process_of_zonebuf zonebuf in
     let processor = (processor_of_process process :> (module PROCESSOR)) in
-    S.listen_udpv4 s listening_port (
-      fun ~src ~dst ~src_port buf ->
-        C.log_s c "got udp"
-        >>= fun () ->
-        let ba = Cstruct.to_bigarray buf in
-        let src' = (Ipaddr.V4 dst), listening_port in
-        let dst' = (Ipaddr.V4 src), src_port in
-        let obuf = (Io_page.get 1 :> Dns.Buf.t) in
-        process_query ba (Dns.Buf.length ba) obuf src' dst' processor
-        >>= function
-        | None ->
-          C.log_s c "No response"
-        | Some rba ->
-          let rbuf = Cstruct.of_bigarray rba in
-          U.write ~source_port:listening_port ~dest_ip:src ~dest_port:src_port udp rbuf
-    );
+    S.listen_udpv4 s listening_port (listener ~processor);
     S.listen s
 end
